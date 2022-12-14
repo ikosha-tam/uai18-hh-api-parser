00b5190e6aecf8a2bb733d841fbdbcf164e39edd
Dmitry Ikonnikov <ikosha-tam@yandex.ru>
2022-12-14 22:49:40 +0300

    update parser_hh.py and .gitignore

--- parser_hh.py
+++ parser_hh.py
@@ -2,5 +2,14 @@ import pprint
 import requests
 
 
-
-
+DOMAIN = "https://api.hh.ru/"
+result = requests.get(DOMAIN)
+print(result.status_code)
+url_vacancies = f"{DOMAIN}vacancies"
+params = {
+    'text': 'junior Python developer',
+    'page': 1
+}
+result = requests.get(url_vacancies, params=params)
+print(result.status_code)
+pprint.pprint(result.json())
