51048fa3961be90bc23fc89dd5e6def8abde7752
Dmitry Ikonnikov <ikosha-tam@yandex.ru>
2022-12-15 14:57:08 +0300

    parser_hh.py update

--- parser_hh.py
+++ parser_hh.py
@@ -1,15 +1,43 @@
+# import json
 import pprint
 import requests
 
 
 DOMAIN = "https://api.hh.ru/"
-result = requests.get(DOMAIN)
-print(result.status_code)
+
 url_vacancies = f"{DOMAIN}vacancies"
+# Название искомой вакансии
+name_vacanie = 'junior Python developer'
+# Параметры запроса
 params = {
-    'text': 'junior Python developer',
+    'text': name_vacanie,
+    # Страница
     'page': 1
 }
-result = requests.get(url_vacancies, params=params)
-print(result.status_code)
-pprint.pprint(result.json())
+# Получаем вакансии
+try:
+    result = requests.get(url_vacancies, params=params)
+    result.raise_for_status()
+    vacancies = result.json()
+except (requests.RequestException, ValueError):
+    print('Ошибка получения данных!!!')
+# Сохраняем вакансии в файл json
+with open('vacancies.json', 'w') as f:
+    f.write(result.text)
+# Число найденных вакансий
+number_vacancies = vacancies['found']
+print(f'Найдено вакансий: {number_vacancies}')
+
+items = vacancies['items']
+print(type(items))
+first = items[0]
+pprint.pprint(first)
+print()
+print('Вакансия: ', first['name'])
+print('url hh: ', first['alternate_url'])
+print('url api: ', first['url'])
+print('-' * 30)
+
+one_vacancie_url = first['url']
+result = requests.get(one_vacancie_url, params=params).json()
+pprint.pprint(result)
